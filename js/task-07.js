d5c352d427c15a233b27d90ce5753a00e4d9d837
AlexandraCherepania <106696097+AlexandraCherepania@users.noreply.github.com>
2022-10-13 09:58:12 +0300

    Update

--- js/task-07.js
+++ js/task-07.js
@@ -1,8 +1,12 @@
- const controlEl = document.querySelector("#font-size-control");
+const inputTextRef = document.querySelector("#text");
+const controlEl = document.querySelector("#font-size-control");
 console.log(controlEl);
 
 controlEl.addEventListener("input", () => {
 const rangeVarEl = controlEl.value;
     console.log(rangeVarEl)
+    inputTextRef.style.fontSize = controlEl.value + 'px';
+
 });
+
 
